18736bdc8186acd28679c27713eae8f1954d0aa3
PartyDonut <PartyDonut@users.noreply.github.com>
2024-10-19 15:06:18 +0200

    chore: improved dialog widgets

--- lib/util/adaptive_layout.dart
+++ lib/util/adaptive_layout.dart
@@ -186,8 +186,8 @@ class _AdaptiveLayoutBuilderState extends ConsumerState<AdaptiveLayoutBuilder> {
   Widget build(BuildContext context) {
     return MediaQuery(
       data: MediaQuery.of(context).copyWith(
-        padding: isDesktop ? const EdgeInsets.only(top: defaultTitleBarHeight) : null,
-        viewPadding: isDesktop ? const EdgeInsets.only(top: defaultTitleBarHeight) : null,
+        padding: isDesktop ? const EdgeInsets.only(top: defaultTitleBarHeight, bottom: 16) : null,
+        viewPadding: isDesktop ? const EdgeInsets.only(top: defaultTitleBarHeight, bottom: 16) : null,
       ),
       child: AdaptiveLayout(
         layout: layout,
