1bf4a797b72951410e9ddf485cd1278c13c61f4f
PartyDonut <PartyDonut@users.noreply.github.com>
2025-02-08 11:21:22 +0100

    feature: Added simple playback information

--- lib/screens/video_player/components/video_playback_information.dart
+++ lib/screens/video_player/components/video_playback_information.dart
@@ -9,8 +9,10 @@ import 'package:fladder/models/playback/playback_model.dart';
 import 'package:fladder/providers/session_info_provider.dart';
 import 'package:fladder/providers/video_player_provider.dart';
 import 'package:fladder/util/clipboard_helper.dart';
+import 'package:fladder/util/humanize_duration.dart';
 import 'package:fladder/util/list_padding.dart';
 import 'package:fladder/util/localization_helper.dart';
+import 'package:fladder/wrappers/players/player_states.dart';
 
 Future<void> showVideoPlaybackInformation(BuildContext context) {
   return showDialog(
@@ -27,6 +29,7 @@ class _VideoPlaybackInformation extends ConsumerWidget {
     final playbackModel = ref.watch(playBackModel);
     final sessionInfo = ref.watch(sessionInfoProvider);
     final backend = ref.read(videoPlayerProvider.select((value) => value.backend));
+    final playbackState = ref.watch(videoPlayerProvider.select((value) => value.lastState));
     return Dialog(
       child: Padding(
         padding: const EdgeInsets.all(12.0),
@@ -76,6 +79,7 @@ class _VideoPlaybackInformation extends ConsumerWidget {
                 ),
               ),
               const Divider(),
+              if (playbackState != null) _PlayerInformation(state: playbackState),
               Text("Playback information", style: Theme.of(context).textTheme.titleMedium),
               const SizedBox(height: 4),
               Padding(
@@ -138,6 +142,57 @@ class _VideoPlaybackInformation extends ConsumerWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class _PlayerInformation extends StatelessWidget {
+  final PlayerState state;
+  const _PlayerInformation({
+    required this.state,
+  });
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Player state", style: Theme.of(context).textTheme.titleMedium),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 4).copyWith(top: 4),
+          child: Opacity(
+            opacity: 0.80,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [const Text('playing: '), Text(state.playing.toString())],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [const Text('buffering: '), Text(state.buffering.toString())],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [const Text('duration: '), Text(state.duration.humanize ?? "")],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [const Text('rate: '), Text(state.rate.toString())],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [const Text('volume: '), Text(state.volume.toString())],
+                ),
+              ].addPadding(const EdgeInsets.symmetric(vertical: 3)),
+            ),
+          ),
+        ),
+        const Divider(),
+      ],
     );
   }
 }
