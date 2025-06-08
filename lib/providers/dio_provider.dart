b4180d6de34998ac3b1e07fe9dcadbbcde11fb59
xKeyBax <sopromasterbox@gmail.com>
2025-06-08 12:34:26 +0200

    Create dio_provider.dart

---
+++ lib/providers/dio_provider.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:fladder/providers/sync_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+final dioProvider = Provider<Dio>((ref) {
+  final dio = Dio();
+  final apiKey = ref.read(apiKeyProvider);
+  dio.interceptors.add(
+    InterceptorsWrapper(
+      onRequest: (options, handler) {
+        options.headers['X-Emby-Token'] = apiKey;
+        return handler.next(options);
+      },
+    ),
+  );
+  return dio;
+});
