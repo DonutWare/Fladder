f2c001b4cbdf5866963c379bb80ad2b34c4b163c
Ulises M <ulises@lbux.org>
2025-08-01 12:58:39 -0700

    implement feedback

--- lib/models/item_base_model.dart
+++ lib/models/item_base_model.dart
@@ -87,7 +87,11 @@ class ItemBaseModel with ItemBaseModelMappable {
                 color: Colors.yellowAccent,
               ),
               const SizedBox(width: 6),
+<<<<<<< HEAD
               Text(overview.communityRating?.toStringAsFixed(2) ?? "--"),
+=======
+              Text(overview.communityRating?.toStringAsFixed(2) ?? "0.0"),
+>>>>>>> 9088937 (implement feedback)
             ],
           ),
         _ => null,
