d20142a72fba2d1f169599ce1a9ff8ad71dcfaaf
PartyDonut <42371342+PartyDonut@users.noreply.github.com>
2026-01-03 13:45:23 +0100

    feat: TV interface improvements (#681)

--- android/app/src/main/kotlin/nl/jknaapen/fladder/VideoPlayerActivity.kt
+++ android/app/src/main/kotlin/nl/jknaapen/fladder/VideoPlayerActivity.kt
@@ -54,7 +54,7 @@ fun VideoPlayerScreen(
     val leanBackEnabled = leanBackEnabled(LocalContext.current)
     ScreenSaver {
         ExoPlayer { player ->
-            ScaledContent(if (leanBackEnabled) 0.6f else 1f) {
+            ScaledContent(if (leanBackEnabled) 0.75f else 1f) {
                 CustomVideoControls(player)
             }
         }
