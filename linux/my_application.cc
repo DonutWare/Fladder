72de9245f4e57df0663bcccfb475702a4404195d
Aiden Schembri <aidsch0605@outlook.com>
2026-03-15 20:15:01 +0100

    refactor: remove unused import and application name setting

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -110,7 +110,6 @@ static void my_application_init(MyApplication *self) {}
 MyApplication *my_application_new()
 {
 	g_set_prgname("Fladder");
-	g_set_application_name("Fladder");
 	return MY_APPLICATION(g_object_new(my_application_get_type(),
 									   "application-id", APPLICATION_ID,
 									   "flags", G_APPLICATION_NON_UNIQUE,
