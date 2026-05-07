33df2fe440ce88bebcb851a6799154a166d069c0
PartyDonut <42371342+PartyDonut@users.noreply.github.com>
2026-05-07 20:33:30 +0200

    Add simplified overwrite for themes

--- lib/widgets/navigation_scaffold/components/music_player_bar_content.dart
+++ lib/widgets/navigation_scaffold/components/music_player_bar_content.dart
@@ -9,6 +9,7 @@ import 'package:fladder/util/duration_extensions.dart';
 import 'package:fladder/util/fladder_image.dart';
 import 'package:fladder/widgets/navigation_scaffold/components/shared/player_bar_shared.dart';
 import 'package:fladder/widgets/shared/item_actions.dart';
+import 'package:fladder/widgets/shared/theme_overwrite.dart';
 
 class MusicFloatingPlayerBarContent extends StatelessWidget {
   const MusicFloatingPlayerBarContent({
@@ -56,122 +57,131 @@ class MusicFloatingPlayerBarContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(6),
-            child: Row(
-              spacing: 12,
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
+    return ThemeOverwrite(
+        image: item.getPosters?.primary?.imageProvider,
+        child: (context) {
+          return Container(
+            color: Theme.of(context).colorScheme.primary.withAlpha(25),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
                 Expanded(
-                  child: Row(
-                    spacing: 12,
-                    children: [
-                      if (playbackInfo.state == VideoPlayerState.minimized)
-                        FloatingPlayerBarPreview(
-                          showExpandButton: showExpandButton,
-                          onShowExpandButton: onShowExpandButton,
-                          openFullScreenPlayer: openFullScreenPlayer,
-                          child: FladderImage(
-                            image: item.images?.primary,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      Flexible(
-                        child: FloatingPlayerBarTitle(
-                          title: item.title,
-                          subtitle: item.artistNames.isNotEmpty ? item.artistNames.join(', ') : item.album ?? "",
-                          onTap: () => item.navigateTo(context),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Row(
-                    spacing: 4,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      if (constraints.maxWidth > 450)
-                        Flexible(
-                          child: Text(
-                            "${lastPosition.readAbleDuration} / ${playbackInfo.duration.readAbleDuration}",
-                            style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                                  color: Theme.of(context).colorScheme.onSurface.withAlpha(125),
+                  child: Padding(
+                    padding: const EdgeInsets.all(6),
+                    child: Row(
+                      spacing: 12,
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Row(
+                            spacing: 12,
+                            children: [
+                              if (playbackInfo.state == VideoPlayerState.minimized)
+                                FloatingPlayerBarPreview(
+                                  showExpandButton: showExpandButton,
+                                  onShowExpandButton: onShowExpandButton,
+                                  openFullScreenPlayer: openFullScreenPlayer,
+                                  child: FladderImage(
+                                    image: item.images?.primary,
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
+                              Flexible(
+                                child: FloatingPlayerBarTitle(
+                                  title: item.title,
+                                  subtitle:
+                                      item.artistNames.isNotEmpty ? item.artistNames.join(', ') : item.album ?? "",
+                                  onTap: () => item.navigateTo(context),
+                                ),
+                              )
+                            ],
                           ),
                         ),
-                      IconButton(
-                        onPressed: onPrevious,
-                        icon: const Icon(IconsaxPlusBold.previous),
-                      ),
-                      IconButton.filledTonal(
-                        onPressed: onPlayPause,
-                        iconSize: 32,
-                        icon:
-                            playbackInfo.playing ? const Icon(IconsaxPlusBold.pause) : const Icon(IconsaxPlusBold.play),
-                      ),
-                      IconButton(
-                        onPressed: onNext,
-                        icon: const Icon(IconsaxPlusBold.next),
-                      ),
-                    ],
+                        Expanded(
+                          child: Row(
+                            spacing: 4,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              if (constraints.maxWidth > 450)
+                                Flexible(
+                                  child: Text(
+                                    "${lastPosition.readAbleDuration} / ${playbackInfo.duration.readAbleDuration}",
+                                    style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                                          color: Theme.of(context).colorScheme.onSurface.withAlpha(125),
+                                        ),
+                                  ),
+                                ),
+                              IconButton(
+                                onPressed: onPrevious,
+                                icon: const Icon(IconsaxPlusBold.previous),
+                              ),
+                              IconButton.filledTonal(
+                                onPressed: onPlayPause,
+                                iconSize: 32,
+                                icon: playbackInfo.playing
+                                    ? const Icon(IconsaxPlusBold.pause)
+                                    : const Icon(IconsaxPlusBold.play),
+                              ),
+                              IconButton(
+                                onPressed: onNext,
+                                icon: const Icon(IconsaxPlusBold.next),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              IconButton(
+                                onPressed: onToggleShuffle,
+                                icon: Icon(
+                                  IconsaxPlusBold.shuffle,
+                                  color: shuffleEnabled
+                                      ? Theme.of(context).colorScheme.primary
+                                      : Theme.of(context).colorScheme.onSurface.withAlpha(125),
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: onCycleRepeatMode,
+                                icon: Icon(
+                                  repeatMode == AudioRepeatMode.one
+                                      ? IconsaxPlusBold.repeate_one
+                                      : IconsaxPlusBold.repeate_music,
+                                  color: repeatMode == AudioRepeatMode.off
+                                      ? Theme.of(context).colorScheme.onSurface.withAlpha(125)
+                                      : Theme.of(context).colorScheme.primary,
+                                ),
+                              ),
+                              OverflowView.flexible(
+                                builder: (context, remainingItemCount) => PopupMenuButton(
+                                  iconColor: Theme.of(context).colorScheme.onSurface.withAlpha(125),
+                                  padding: EdgeInsets.zero,
+                                  itemBuilder: (context) => itemActions
+                                      .sublist(itemActions.length - remainingItemCount)
+                                      .map((e) => e.toPopupMenuItem(useIcons: true))
+                                      .toList(),
+                                ),
+                                children: itemActions.map((e) => e.toButton()).toList(),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-                Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      IconButton(
-                        onPressed: onToggleShuffle,
-                        icon: Icon(
-                          IconsaxPlusBold.shuffle,
-                          color: shuffleEnabled
-                              ? Theme.of(context).colorScheme.primary
-                              : Theme.of(context).colorScheme.onSurface.withAlpha(125),
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: onCycleRepeatMode,
-                        icon: Icon(
-                          repeatMode == AudioRepeatMode.one
-                              ? IconsaxPlusBold.repeate_one
-                              : IconsaxPlusBold.repeate_music,
-                          color: repeatMode == AudioRepeatMode.off
-                              ? Theme.of(context).colorScheme.onSurface.withAlpha(125)
-                              : Theme.of(context).colorScheme.primary,
-                        ),
-                      ),
-                      OverflowView.flexible(
-                        builder: (context, remainingItemCount) => PopupMenuButton(
-                          iconColor: Theme.of(context).colorScheme.onSurface.withAlpha(125),
-                          padding: EdgeInsets.zero,
-                          itemBuilder: (context) => itemActions
-                              .sublist(itemActions.length - remainingItemCount)
-                              .map((e) => e.toPopupMenuItem(useIcons: true))
-                              .toList(),
-                        ),
-                        children: itemActions.map((e) => e.toButton()).toList(),
-                      )
-                    ],
-                  ),
-                )
+                FloatingPlayerBarProgress(
+                  playbackInfo: playbackInfo,
+                  lastPosition: lastPosition,
+                  onChangeStart: onChangeStart,
+                  onChanged: onChanged,
+                  onChangeEnd: onChangeEnd,
+                ),
               ],
             ),
-          ),
-        ),
-        FloatingPlayerBarProgress(
-          playbackInfo: playbackInfo,
-          lastPosition: lastPosition,
-          onChangeStart: onChangeStart,
-          onChanged: onChanged,
-          onChangeEnd: onChangeEnd,
-        ),
-      ],
-    );
+          );
+        });
   }
 }
--- lib/widgets/shared/theme_overwrite.dart
+++ lib/widgets/shared/theme_overwrite.dart
@@ -3,50 +3,73 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:fladder/providers/settings/client_settings_provider.dart';
+import 'package:fladder/screens/shared/detail_scaffold.dart';
 import 'package:fladder/theme.dart';
 
 class ThemeOverwrite extends ConsumerWidget {
+  final ImageProvider? image;
   final Color? color;
   final Widget Function(BuildContext) child;
   const ThemeOverwrite({
     super.key,
+    this.image,
     this.color,
     required this.child,
   });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final schemeVariant = ref.watch(clientSettingsProvider.select((value) => value.schemeVariant));
-    final newColorScheme = color != null
-        ? ColorScheme.fromSeed(
-            seedColor: color!,
-            brightness: Theme.brightnessOf(context),
-            dynamicSchemeVariant: schemeVariant,
-          )
-        : null;
-    final amoledBlack = ref.watch(clientSettingsProvider.select((value) => value.amoledBlack));
-    final amoledOverwrite = amoledBlack ? Colors.black : null;
+    final deriveColorFromItem = ref.watch(clientSettingsProvider.select((value) => value.deriveColorsFromItem));
+    if (!deriveColorFromItem) {
+      return child(context);
+    }
 
-    final themeData =
-        newColorScheme != null && ref.watch(clientSettingsProvider.select((value) => value.deriveColorsFromItem))
-            ? FladderTheme.theme(newColorScheme, schemeVariant).copyWith(
-                scaffoldBackgroundColor: amoledOverwrite,
-                cardColor: amoledOverwrite,
-                canvasColor: amoledOverwrite,
-                colorScheme: newColorScheme.copyWith(
-                  surface: amoledOverwrite,
-                  surfaceContainerHighest: amoledOverwrite,
-                  surfaceContainerLow: amoledOverwrite,
-                ),
-              )
-            : Theme.of(context).copyWith(
-                scaffoldBackgroundColor: amoledOverwrite,
-                cardColor: amoledOverwrite,
-                canvasColor: amoledOverwrite,
-              );
+    ThemeData themeData(Color? color) {
+      final schemeVariant = ref.watch(clientSettingsProvider.select((value) => value.schemeVariant));
+      final newColorScheme = color != null
+          ? ColorScheme.fromSeed(
+              seedColor: color,
+              brightness: Theme.brightnessOf(context),
+              dynamicSchemeVariant: schemeVariant,
+            )
+          : null;
+      final amoledBlack = ref.watch(clientSettingsProvider.select((value) => value.amoledBlack));
+      final amoledOverwrite = amoledBlack ? Colors.black : null;
+
+      return newColorScheme != null
+          ? FladderTheme.theme(newColorScheme, schemeVariant).copyWith(
+              scaffoldBackgroundColor: amoledOverwrite,
+              cardColor: amoledOverwrite,
+              canvasColor: amoledOverwrite,
+              colorScheme: newColorScheme.copyWith(
+                surface: amoledOverwrite,
+                surfaceContainerHighest: amoledOverwrite,
+                surfaceContainerLow: amoledOverwrite,
+              ),
+            )
+          : Theme.of(context).copyWith(
+              scaffoldBackgroundColor: amoledOverwrite,
+              cardColor: amoledOverwrite,
+              canvasColor: amoledOverwrite,
+            );
+    }
+
+    if (image != null) {
+      return FutureBuilder<Color?>(
+        future: getDominantColor(image!),
+        builder: (context, snapshot) {
+          return Theme(
+            data: themeData(snapshot.data),
+            child: Builder(
+              builder: (context) => child(context),
+            ),
+          );
+        },
+      );
+    }
 
     return Theme(
-      data: themeData,
+      data: themeData(color),
       child: Builder(
         builder: (context) => child(context),
       ),
